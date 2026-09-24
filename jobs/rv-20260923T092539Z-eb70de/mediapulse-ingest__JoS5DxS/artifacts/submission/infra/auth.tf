4dad6e733e80d19288b41e9ccd9cd5d1a1a1671d
azeezsayyad <azeezahmad.sd@gmail.com>
2026-09-24 23:51:02 +0530

    fix

--- jobs/rv-20260923T092539Z-eb70de/mediapulse-ingest__JoS5DxS/artifacts/submission/infra/auth.tf
+++
@@ -1,4 +0,0 @@
-resource "google_identity_platform_config" "default" {
-  provider = google-beta
-  project  = var.gcp_project_id
-}
--- jobs/rv-20260923T101646Z-7df111/mediapulse-ingest__7Bq74UY/artifacts/submission/infra/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
-provider "google-beta" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
